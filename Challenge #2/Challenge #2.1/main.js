f043b8d5d46dae0fd885196f6599727bdda1823e
sjteranc <teran.susanjhoseline@gmail.com>
2024-07-11 18:14:19 -0400

    Complete Challenge 2.2

--- Challenge #2/Challenge #2.1/main.js
+++ Challenge #2/Challenge #2.1/main.js
@@ -1,46 +1,31 @@
-
 function findSmallestNumber(arr) {
-    if (arr.length === 0) return null;
-    let smallest = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < smallest) {
-            smallest = arr[i];
-        }
-    }
-    return smallest;
+    if (!arr.length) return null;
+    return arr.reduce((smallest, num) => num < smallest ? num : smallest, arr[0]);
 }
 
 function findLeastFrequentItem(arr) {
-    if (arr.length === 0) return null;
-    let counts = {};
-    for (let item of arr) {
-        counts[item] = (counts[item] || 0) + 1;
-    }
-    let leastFrequentItem;
-    let minCount = Infinity;
-    for (let item in counts) {
-        if (counts[item] < minCount) {
-            minCount = counts[item];
-            leastFrequentItem = item;
-        }
-    }
-    return leastFrequentItem;
+    if (!arr.length) return null;
+    const counts = arr.reduce((acc, item) => {
+        acc[item] = (acc[item] || 0) + 1;
+        return acc;
+    }, {});
+
+    const minCount = Math.min(...Object.values(counts));
+    return Object.keys(counts).find(item => counts[item] === minCount);
 }
 
 function removeDuplicates(arr) {
-    return [...new Set(arr)];
+    return arr.filter((item, index) => arr.indexOf(item) === index);
 }
 
-
 function main() {
-    let arr1 = [12, 6, 10, 2, 45, 100];
-    let arr2 = [3, 'c', 'c', 'a', 2, 3, 'c', 3, 'c', 2, 4, 9, 9];
-    let arr3 = [7, 9, 1, 'a', 'a', 'f', 9 , 4, 2, 'd', 'd'];
+    const arr1 = [12, 6, 10, 2, 45, 100];
+    const arr2 = [3, 'c', 'c', 'a', 2, 3, 'c', 3, 'c', 2, 4, 9, 9];
+    const arr3 = [7, 9, 1, 'a', 'a', 'f', 9 , 4, 2, 'd', 'd'];
 
     console.log("Smallest number in arr1:", findSmallestNumber(arr1)); 
     console.log("Least frequent item in arr2:", findLeastFrequentItem(arr2)); 
     console.log("Array with duplicates removed from arr3:", removeDuplicates(arr3)); 
 }
-
 
 main();
---
+++ Challenge #2/Challenge #2.2/main.js
@@ -0,0 +1,30 @@
+function removeDuplicates(arr) {
+    return arr.reduce((unique, item) => {
+        if (!unique.includes(item)) {
+            unique.push(item);
+        }
+        return unique;
+    }, []);
+}
+
+function concatenateArrays(arr) {
+    return arr.reduce((result, subArray) => {
+        result.push(subArray.join(' '));
+        return result;
+    }, []);
+}
+
+function main() {
+    let arr2 = [7, 9, 1, 'a', 'a', 'f', 9 , 4, 2, 'd', 'd'];
+    let data = [
+        ["The", "little", "horse"],
+        ["Plane", "over", "the", "ocean"],
+        ["Chocolate", "ice", "cream", "is", "awesome"],
+        ["this", "is", "a", "long", "sentence"]
+    ];
+
+    console.log("Array with duplicates removed:", removeDuplicates(arr2));
+    console.log("Concatenated arrays:", concatenateArrays(data));
+}
+
+main();
